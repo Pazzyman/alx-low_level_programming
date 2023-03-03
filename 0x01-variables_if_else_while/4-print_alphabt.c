8609b5b4b0cb523b1ee64af06cb7783c8337525d
Pazzyman <ibekweekene49@gmail.com>
2023-03-03 14:32:07 -0800

    finishing

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,9 @@
+
 #include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: prints the alphabet in lowercase\n
+ * Description: prints lowercase alphabets
  * Return: Always 0 (success)
  */
 int main(void)
@@ -11,8 +12,14 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
 	}
 	putchar('\n');
-return (0);
+	return (0);
 }
